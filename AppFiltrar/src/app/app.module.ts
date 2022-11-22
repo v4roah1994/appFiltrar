c2839adf078c306cf39a09e3df7d807a74526a46
Yina Morales <yinmorales@uniboyaca.edu.co>
2022-11-22 14:50:46 -0500

    Importacion modulo Client

--- AppFiltrar/src/app/app.module.ts
+++ AppFiltrar/src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+//npm install 
+//import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   declarations: [AppComponent],
